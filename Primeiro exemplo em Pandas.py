68ae954bdeffa1c9f249cd858eee9bdd3e0f7899
TulioZY <tulio.correia.tcr@gmail.com>
2023-02-06 22:07:43 -0300

    Exemplo aula 1

---
+++ Primeiro exemplo em Pandas.py
@@ -0,0 +1,9 @@
+# Primeiro exemplo de manipulação do Pandas
+# Autor: Túlio Correia
+# Data: 6 de Fevereiro de 2023
+
+
+# Importando biblioteca Pandas no Python
+import pandas as pd
+
+combustiveis_df = pd.read_csv("precos-semestrais-ca-2022-01.csv")
